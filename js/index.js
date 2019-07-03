61dfde1bbf15590944e619e227dfde94ffe9ae33
Roman <tsvetkovv22@gmail.com>
2019-07-03 10:10:14 +1000

    Подготовка для обновления

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
-window.sr = ScrollReveal();
-sr.reveal('.anim');
+// window.sr = ScrollReveal();
+// sr.reveal('.anim');
 
-const menuButton = document.querySelector('.menu-btn');
-const menuItems = document.querySelectorAll('.menu-item');
-const menu = document.querySelector('.menu');
+// const menuButton = document.querySelector('.menu-btn');
+// const menuItems = document.querySelectorAll('.menu-item');
+// const menu = document.querySelector('.menu');
 
-menuButton.addEventListener('click', () => {
-  menuButton.classList.toggle('menu-btn_active');
-  menu.classList.toggle('menu_active');
-});
+// menuButton.addEventListener('click', () => {
+//   menuButton.classList.toggle('menu-btn_active');
+//   menu.classList.toggle('menu_active');
+// });
 
-Array.from(menuItems).forEach(el => {
-  el.addEventListener('click', () => {
-    menuButton.classList.remove('menu-btn_active');
-    menu.classList.remove('menu_active');
-  });
-});
+// Array.from(menuItems).forEach(el => {
+//   el.addEventListener('click', () => {
+//     menuButton.classList.remove('menu-btn_active');
+//     menu.classList.remove('menu_active');
+//   });
+// });
